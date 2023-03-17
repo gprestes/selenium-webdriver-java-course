a5f1137c374b8ede65f8c5858e17973a3f058272
Guilherme Heuser Prestes <gprestes@thoughtworks.com>
2023-03-17 12:47:17 +0100

    Add buildevent subspan and context

--- webdriver_java/resources/test_result_script.sh
+++ webdriver_java/resources/test_result_script.sh
@@ -17,3 +17,9 @@ github_actions_passed_tests{action_id="${GITHUB_RUN_NUMBER}", commit="${GITHUB_S
 github_actions_failed_tests{action_id="${GITHUB_RUN_NUMBER}", commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')
 github_actions_skipped_tests{action_id="${GITHUB_RUN_NUMBER}", commit="${GITHUB_SHA}", actor="${GITHUB_ACTOR}", branch="${GITHUB_REF}"} $(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')
 EOF
+
+echo " gha.maventest.ignore=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $2 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.total=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $4 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.passed=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $6 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.failed=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $8 }')" >> $BUILDEVENT_FILE
+echo " gha.maventest.skipped=$(echo ${TEST_RESULTS_STRING} | awk -F'"' '{ print $10 }')" >> $BUILDEVENT_FILE
